30115b1fb589e8fa4a62a978e728cde85670875e
Carlin Eng <carlineng@gmail.com>
2011-03-10 13:43:47 -0800

    Updated scratchpad and data.  Grabbed some of the unique values for exploratory analysis.

--- scratch.R
+++ scratch.R
@@ -3,6 +3,8 @@ source("queryAPI.R")
 library(plyr)
 library(ggplot2)
 
+load("scratch.RData")
+
 # Scratchwork just to play with the data
 # Data ranges from 1984-2010
 
@@ -20,12 +22,18 @@ receiptAgg <- getReceiptAggregate()
 acct.unique <- unique(budgetAcct["account"]) # 1580 unique vals
 agency.unique <- unique(budgetAcct["agency"]) # 26 unique vals
 bureau.unique <- unique(budgetAcct["bureau"]) # 326 unique vals
-# function, subfunction, onbudget
-
+# function, subfunction
+fn.unique <- unique(budgetAcct["function"])
+subfn.unique <- unique(budgetAcct["subfunction"])
 # accountID, agencyID, bureauID, functionID, subfunctionID
-
-# category, subcategory, accountID
-
+acct.id.unique <- unique(budgetAcct["accountID"])
+agency.id.unique <- unique(budgetAcct["agencyID"])
+bureau.id.unique <- unique(budgetAcct["bureauID"])
+fn.id.unique <- unique(budgetAcct["function"])
+subfn.id.unique <- unique(budgetAcct["subfunction"])
+# category, subcategory
+cat.unique <- unique(receiptAcct["category"])
+subcat.unique <- unique(receiptAcct["subcategory"])
 # Get pop/GDP/debt/inflation
 population <- getPopulation(startYear=1984, endYear=2010)
 GDP <- getGDP(startYear=1984, endYear=2010)
